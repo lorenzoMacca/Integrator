0d8df9bc3ec15a325a6573ebbf17771d79618806
lorenzo cozza <lorenzo.cozza@hotmail.com>
2016-11-27 13:49:00 +0100

    inizializzazione shot term plan

--- Integrator/gra/ui_stp/ui_define_stp.cpp
+++ Integrator/gra/ui_stp/ui_define_stp.cpp
@@ -8,18 +8,58 @@ UIDefineSTP::UIDefineSTP(Data *d, QWidget *parent) :
     ui->setupUi(this);
     this->m_data = d;
 
-    this->ui->m_ui_cw_2->setText(QString::number(QDate::currentDate().weekNumber()));
+    IntegrationPlan* i = this->m_data->integrationPlan();
+
+    this->ui->m_ui_cw_2->setText(i->cw());
+    this->ui->m_ui_start_date->setDate(i->start_date());
+    this->ui->m_ui_due_date->setDate(i->due_date());
+    this->ui->m_ui_merge_date->setDate(i->merge_date());
+
+    this->ui->m_isMerge->setChecked(!i->noMerge());
+    if(i->noMerge())
+    {
+        this->ui->m_ui_merge_date->setDisabled(true);
+    }else
+    {
+        this->ui->m_ui_merge_date->setDisabled(false);
+    }
+
     this->ui->m_ui_sub_project->addItems(this->m_data->settingData()->subProjects());
     this->ui->m_ui_assignee->addItems(this->m_data->settingData()->assignees());
     this->ui->m_ui_pic->addItems(this->m_data->settingData()->pics());
     this->ui->m_ui_buildtype->addItems(this->m_data->settingData()->stp_types());
-    this->ui->m_ui_start_date->setDate(QDate::currentDate());
-    this->ui->m_ui_due_date->setDate(QDate::currentDate());
-    this->ui->m_ui_merge_date->setDate(QDate::currentDate());
 	this->ui->m_ui_security_level->addItems(this->m_data->settingData()->securityLevels());
-    this->ui->m_ui_merge_date->setDisabled(false);
-    this->ui->m_isMerge->setChecked(true);
-	
+
+    int i1 = this->ui->m_ui_sub_project->findText(i->sub_project());
+    if(i1 > -1)
+    {
+        this->ui->m_ui_sub_project->setCurrentIndex(i1);
+    }
+
+    int i2 = this->ui->m_ui_assignee->findText(i->assignees());
+    if(i2 > -1)
+    {
+        this->ui->m_ui_assignee->setCurrentIndex(i2);
+    }
+
+    int i3 = this->ui->m_ui_pic->findText(i->assignees());
+    if(i3 > -1)
+    {
+        this->ui->m_ui_pic->setCurrentIndex(i3);
+    }
+
+    int i4 = this->ui->m_ui_buildtype->findText(i->stp_type());
+    if(i4 > -1)
+    {
+        this->ui->m_ui_buildtype->setCurrentIndex(i4);
+    }
+
+    int i5 = this->ui->m_ui_security_level->findText(i->security_level());
+    if(i5 > -1)
+    {
+        this->ui->m_ui_security_level->setCurrentIndex(i5);
+    }
+
 
     connect(this->ui->m_next_button, SIGNAL(clicked()),
             this, SLOT(nextButtonClicketSlot()));
